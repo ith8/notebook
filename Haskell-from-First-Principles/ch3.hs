95d40f7cc34e25950d8ea5544a086bb5f6d5a451
TSH <foo-san@protonmail.com>
2018-12-23 14:51:47 +0000

    Update ch3.hs

--- Haskell-from-First-Principles/ch3.hs
+++ Haskell-from-First-Principles/ch3.hs
@@ -18,16 +18,16 @@
  apply f a = f a
    where f a = (a,a)
 -- 3.
-second :: [a] -> a
-second xs = head (tail xs)
-swap :: (a,b) -> (b,a)
-swap (x,y) = (y,x)
-pair :: a -> b -> (a,b)
-pair x y = (x,y)
-double :: Num a => a -> a
-palindrome :: Eq a => [a] -> Bool --
-palindrome xs = reverse xs == xs
-twice :: ( a -> a ) -> a -> a
-twice f x = f (f x)
+ second :: [a] -> a
+ second xs = head (tail xs)
+ swap :: (a,b) -> (b,a)
+ swap (x,y) = (y,x)
+ pair :: a -> b -> (a,b)
+ pair x y = (x,y)
+ double :: Num a => a -> a
+ palindrome :: Eq a => [a] -> Bool --
+ palindrome xs = reverse xs == xs
+ twice :: ( a -> a ) -> a -> a
+ twice f x = f (f x)
 {- 5. Function types are not instances of the Eq class since in order to compare two function, and verify whether they are equal, you would have to test all possible inputs (which may be infinite) and see if all their outputs are equal. It may be feasible for Function types to be instances of the Eq class if the set of inputs are limited. For example, only Int between 2 and 5, or only Char before 'g'. 
 -}
